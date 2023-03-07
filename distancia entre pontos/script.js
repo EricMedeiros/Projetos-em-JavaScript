990f0916aa1782b2b8c6a9a80cf2f4a69e2ce165
Eric Medeiros Alves <124798538+EricMedeiros@users.noreply.github.com>
2023-03-06 21:50:31 -0300

    projeto terminado

--- distancia entre pontos/script.js
+++ distancia entre pontos/script.js
@@ -1,11 +1,11 @@
 function calcular(){
-var p1x = document.getElementById('p1x');
-var p1y = document.getElementById('p1y');
-var p2x = document.getElementById('p2x');
-var p2y = document.getElementById('p2y');
-var a = p2x - p1x ;
-var b = p2y - p1y;
-var soma =Number.parseInt(Math.sqrt(a*a - b*b));
-var res = document.getElementById('res');
-res.innerHTML += `a distância entre os pontos é ${soma.value}`;
+var p1x = document.getElementById('p1x')
+var p1y = document.getElementById('p1y')
+var p2x = document.getElementById('p2x')
+var p2y = document.getElementById('p2y')
+var a = (p2x.value - p1x.value)
+var b = (p2y.value - p1y.value)
+var soma =Math.sqrt(a*a + b*b)
+var res = document.getElementById('res')
+res.innerHTML = `a distância entre os pontos é ${soma}`;
 };
