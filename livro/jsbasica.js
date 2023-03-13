585888b9b4d8b8bb7e89a7a5f1bc089397c203ea
Eric Medeiros Alves <124798538+EricMedeiros@users.noreply.github.com>
2023-03-13 17:48:55 -0300

    treinando setInterval e setTimeout

--- livro/jsbasica.js
+++ livro/jsbasica.js
@@ -27,4 +27,22 @@ while(n>1){
 console.log(produto)
 
 var hoje = new Date();
+
 console.log(hoje.getHours())
+
+var x =0
+var looping = setInterval(function(){
+   console.log(x)
+  
+   x++;
+   /*if(x>10){
+   clearInterval(looping);
+   console.log('finished');
+}*/
+}, 500 );
+
+setTimeout(function(){
+   console.log('se passaram 2 segundos!!!');
+   clearInterval(looping);
+},2000)
+
